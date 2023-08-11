041c6d7dec458231d4c3da0d4a3d026095b40060
Klinton-VM <100642525+Klintonjmd@users.noreply.github.com>
2023-08-11 14:53:40 -0400

    Add buttons and score display

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const score = document.querySelector("#score");
+let computerScore = 0;
+let playerScore = 0;
+score.textContent = playerScore + " - " + computerScore;
+
+rock.addEventListener("click", function (e) {
+  playRound("rock", getComputerChoice());
+});
+
+paper.addEventListener("click", function (e) {
+  playRound("paper", getComputerChoice());
+});
+
+scissors.addEventListener("click", function (e) {
+  playRound("scissors", getComputerChoice());
+});
+
 function getComputerChoice() {
   let val = Math.floor(Math.random() * 3) + 1;
   let res = "Scissors";
@@ -13,47 +33,26 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    console.log("It's a tie");
-    return 1;
+    return;
   } else if (playerSelection === "rock") {
     if (computerSelection === "scissors") {
-      console.log("You win! Rock beats Scissors");
-      return 1;
+      playerScore += 1;
     } else {
-      console.log("You lose! Paper beats Rock");
-      return 0;
+      computerScore += 1;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "rock") {
-      console.log("You win! Paper beats Rock");
-      return 1;
+      playerScore += 1;
     } else {
-      console.log("You lose! Scissors beats Paper");
-      return 0;
+      computerScore += 1;
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "paper") {
-      console.log("You win! Scissors beats Paper");
-      return 1;
+      playerScore += 1;
     } else {
-      console.log("You lose! Rock beats Scissors");
-      return 0;
+      computerScore += 1;
     }
   }
+  score.innerText = playerScore + " - " + computerScore;
+  return;
 }
-
-function match() {
-  let playerWins = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("What is your choice?");
-    playerWins += playRound(playerSelection, getComputerChoice());
-    console.log(playerWins);
-  }
-  if (playerWins >= 3) {
-    console.log("You won the match!");
-  } else {
-    console.log("You lost the match");
-  }
-}
-
-console.log(match());
